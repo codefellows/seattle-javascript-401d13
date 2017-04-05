f150ae8f451a8c2f13b276f1055f545ee1fa987f
bnates <brian.nations@codefellows.com>
2017-04-05 14:12:10 -0700

    added webpack 2 and karma configs

--- 31-testing_components/demo/cf-gram-testing-components/webpack.config.js
+++ 31-testing_components/demo/cf-gram-testing-components/webpack.config.js
@@ -3,67 +3,43 @@
 const dotenv = require('dotenv');
 const webpack = require('webpack');
 const HTMLPlugin = require('html-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const production = process.env.NODE_ENV === 'production';
 
 dotenv.load();
 
-let plugins = [
-  new ExtractTextPlugin('bundle.css'),
-  new HTMLPlugin({ template: `${__dirname}/app/index.html` }),
-  new webpack.DefinePlugin({
-    __API_URL__: JSON.stringify(process.env.API_URL),
-    __DEBUG__: JSON.stringify(!production)
-  })
-];
-
-if (production) {
-  plugins = plugins.concat([
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: true,
-      compress: {
-        warnings: false
-      },
-    }),
-    new CleanPlugin()
-  ]);
-};
-
 module.exports = {
+  devtool: 'eval',
   entry: `${__dirname}/app/entry.js`,
-  devtool: production ? false : 'eval',
-  plugins,
   output: {
-    path: 'build',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: `${__dirname}/build`
   },
-  sassLoader: {
-    includePaths: [`${__dirname}/app/scss/`]
-  },
+  plugins: [
+    new HTMLPlugin({
+      template: `${__dirname}/app/index.html`
+    }),
+    new ExtractTextPlugin('bundle.css'),
+    new webpack.DefinePlugin({
+      __API_URL__: JSON.stringify(process.env.API_URL),
+      __DEBUG__: JSON.stringify(!production)
+    })
+  ],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'babel-loader'
       },
       {
         test: /\.html$/,
-        loader: 'html'
-      },
-      {
-        test: /\.(woff|tt|svg|eot).*/,
-        loader: 'url?limit=10000&name=image/[hash].[ext]'
-      },
-      {
-        test: /\.(jpg|jpeg|svg|bmp|tiff|gif|png)$/,
-        loader: 'url?limit=10000&name=image/[hash].[ext]'
+        loader: 'html-loader'
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', 'css!resolve-url!sass?sourceMap')
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
       }
     ]
   }
